919c3bf51972254e48a9ea1dee22f5749bff06b1
Artem Melentyev <artem@melentyev.me>
2022-12-01 19:53:16 +0100

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,16 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    var cur = 0L
+    val list = mutableListOf<Long>()
+    for (s in input) {
+        if (s == "") {
+            list.add(cur)
+            cur = 0
+        } else {
+            cur += s.toLong()
+        }
+    }
+    list.sort()
+    println(list.last())
+    println(list.takeLast(3).sum())
 }
